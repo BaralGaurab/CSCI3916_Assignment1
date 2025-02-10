87562b5e3a407fafe30639a214d5bc64ebe4699e
GaurabBaral <gaurab.baral@ucdenver.edu>
2025-02-09 19:46:40 -0700

    Passing Test for HW 1

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const acceptHeader = req.get('accept');
+
+    const responseBody = {
+        acceptHeader: acceptHeader,
+        ...req.body
+    }
+
+    res.json(responseBody);
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = server;
